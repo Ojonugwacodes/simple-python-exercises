49e78ffa158fc5c939c1ca700ce9e0e9de49e6c1
Omerah Rhema Ojonugwa <144184765+Ojonugwacodes@users.noreply.github.com>
2025-02-28 00:28:29 +0100

    Update tempconverter.py

--- tempconverter.py
+++ tempconverter.py
@@ -1,3 +1,4 @@
+# Simple program that converts user input from Celsius temperature to Fahnreheit temperature
 celsius_temp = input("Temperature in Celsius?") 
 fahnreheit_temp = (float(9/5) * float(celsius_temp)) + 32
 print(fahnreheit_temp)
