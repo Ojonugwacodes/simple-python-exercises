6cf9859c166006efcf5664b1609af8b56c202e31
Rhema Omerah <omerahrhema@gmail.com>
2025-02-28 00:00:20 +0100

    initial commit

---
+++ input2.py
@@ -0,0 +1,17 @@
+def compute_pay(hours,rate):
+  try:
+    hours = float(hours)
+    rate = float(rate)
+    if hours > 40:
+      overtime_hours = hours - 40
+      overtime_pay = rate * 1.5
+      pay = (overtime_hours) * (overtime_pay)  + 40 * rate
+    else:
+      pay = hours * rate
+    return pay
+  except(ValueError, TypeError):
+    return'Error'
+hours = input("Enter hours:")
+rate = input("Enter rate:")
+result = compute_pay(hours,rate)
+print("Pay", result)
---
+++ tempconverter.py
@@ -0,0 +1,3 @@
+celsius_temp = input("Temperature in Celsius?") 
+fahnreheit_temp = (float(9/5) * float(celsius_temp)) + 32
+print(fahnreheit_temp)
