c997097caab170021d5c3509cfef619346c91690
Omerah Rhema Ojonugwa <144184765+Ojonugwacodes@users.noreply.github.com>
2025-02-28 00:21:03 +0100

    Update input.py

--- input.py
+++ input.py
@@ -1,2 +1,4 @@
+# Simple program that prompts a user to input a name.
 name = input("What is your name?")
+# "Name" is stored as a variable and then the program prints a welcome message using "name" as a return value
 print(f"Welcome, {name}")
